9ad164bdcdfd05cc9324a68922eac74fcd0c66a8
XueliangZhu <xlzhu@xlzhu.local>
2017-02-15 13:01:36 +1100

    rename and delete some unused code

---
+++ VIPER-Douban/SuggestionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2017 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class SuggestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
