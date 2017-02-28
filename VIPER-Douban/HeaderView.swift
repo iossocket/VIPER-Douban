07644612f9745c9b6e36341845fea78744775e54
iossocket <avx302@gmail.com>
2017-02-28 17:42:03 +1100

    add page control.

--- VIPER-Douban/HeaderView.swift
+++ VIPER-Douban/HeaderView.swift
@@ -8,15 +8,13 @@
 
 import UIKit
 
-class HeaderView: UIView {
+class HeaderView: UIView, UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     private var pageControl: UIPageControl?
  
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         pageControl = UIPageControl()
-        pageControl?.numberOfPages = 5
-        pageControl?.currentPage = 1
         pageControl?.translatesAutoresizingMaskIntoConstraints = false
         addSubview(pageControl!)
         pageControl?.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
@@ -27,6 +25,9 @@ class HeaderView: UIView {
     
     func config(images: Array<String>) {
         scrollView.isPagingEnabled = true
+        scrollView.delegate = self
+        pageControl?.numberOfPages = images.count
+        pageControl?.currentPage = 0
         let imageViews = images.map { name -> UIImageView in
             let view = UIImageView(image: UIImage(named: name))
             view.contentMode = UIViewContentMode.scaleAspectFill
@@ -46,5 +47,11 @@ class HeaderView: UIView {
                imageView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
             }
         }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let xOffset = scrollView.contentOffset.x
+        let page = Int(xOffset / self.bounds.width)
+        pageControl?.currentPage = page
     }
 }
